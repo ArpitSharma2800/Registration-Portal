9bb3c2722aac7e54f102aef9590727052df075c3
ArpitSharma2800 <28arpit2000sharma@gmail.com>
2020-01-13 11:23:49 +0530

    Updated and responsive

--- src/App.js
+++ src/App.js
@@ -1,47 +1,64 @@
 import React, { Component } from 'react';
 import LGOG from './workshop.svg'
 import LGO from './download.png'
+import fire from './fire';
+import Recaptcha from 'react-recaptcha'
+import SUC from './success.gif'
 class App extends Component {
   state = {
-    name: '',
-    email: '',
-    reg: '',
-    year: '',
-    toggle: ''
+    name: null,
+    email: null,
+    reg: null,
+    year: null,
+    toggle: null,
+    loaded: false,
+    img: false
+  }
+
+  verifyCallback = e => {
+    this.setState({
+      loaded: true
+    })
+  }
+
+  Callback() {
+    console.log('You aret not a robot')
   }
   onSubmit = (e) => {
     e.preventDefault();
-
     this.setState({
+      img: true
+    })
+    const newItem = {
       name: e.target.name.value,
       email: e.target.email.value,
       reg: e.target.reg.value,
       year: e.target.se.value,
-      toggle: e.target.toggle.value
-    })
-    const name = this.refs.email.value;
-    console.log('Your name is', name);
+      toggle: e.target.toggle.value,
+    }
+    let messageRef = fire.database().ref('message').orderByKey().limitToLast(100);
+    fire.database().ref('message').push(newItem);
   }
   render() {
     return (
       <div className="App m-5">
         <div className="img1"><img src={LGO} alt="Avatar" /></div>
-        <div className="text-white"><h3>Google DSC is back!!</h3>
+        <div className="text-white">
 
           <div><img src={LGOG} alt="Avatar" /></div>
         </div>
-        <form className="cards p-3" onSubmit={this.onSubmit}>
+        <form className="cards top p-3" onSubmit={this.onSubmit} >
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Name</label>
             <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Your Name" name="name" />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address" ref="email" name="email" />
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address" ref="email" name="email" required pattern="^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$" />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Registration Number</label>
-            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="" name="reg" />
+            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Registration Number" name="reg" />
           </div>
           <div>
             <label htmlFor="exampleInputPassword1">Year</label>
@@ -56,10 +73,38 @@ class App extends Component {
             <input type="checkbox" className="custom-control-input" id="customSwitch1" name="toggle" />
             <label className="custom-control-label" htmlFor="customSwitch1">Have you ever attended Our previous Workshop</label>
           </div>
-          {/* <div>{this.state.email}</div> */}
           <br></br>
-          <button type="submit" className="btn btn-outline-success" >Submit</button>
-        </form>
+          <div className="cap">
+            <Recaptcha
+              sitekey=""
+              render="explicit"
+              theme="light"
+              size="compact"
+              onloadCallback={this.Callback}
+              verifyCallback={this.verifyCallback}
+            />
+          </div>
+          <br></br>
+          {
+            this.state.loaded ? (
+              <div>
+                <button type="submit" className="btn btn-light" >Submit</button>
+              </div>
+            ) : (
+                <div></div>
+              )
+          }
+          {
+            this.state.img ? (
+              <div className="animation">
+                <img src={SUC} alt="Avatar" />
+              </div>
+            ) : (
+                <div></div>
+              )
+          }
+
+        </form >
       </div >
 
     );
---
+++ src/fire.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase'
+var firebaseConfig = {
+
+};
+
+var fire = firebase.initializeApp(firebaseConfig)
+export default fire;
