146b2b1f877a559db6569573af527fc33ce3901d
ArpitSharma2800 <28arpit2000sharma@gmail.com>
2020-01-14 20:19:45 +0530

    Registration Portal

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import LGOG from './workshop.svg'
 import LGO from './download.png'
 import fire from './fire';
 import Recaptcha from 'react-recaptcha'
-import SUC from './success.gif'
+import SUC from './gg.gif'
+
 class App extends Component {
   state = {
     name: null,
@@ -11,14 +12,25 @@ class App extends Component {
     reg: null,
     year: null,
     toggle: null,
+    num: null,
     loaded: false,
+    comment: null,
+    gender: null,
     img: false
   }
-
-  verifyCallback = e => {
+  fclick = e => {
     this.setState({
-      loaded: true
+
     })
+  }
+  verifyCallback = e => {
+    console.log(e);
+    if (e) {
+      this.setState({
+        loaded: true
+      })
+    }
+
   }
 
   Callback() {
@@ -27,7 +39,9 @@ class App extends Component {
   onSubmit = (e) => {
     e.preventDefault();
     this.setState({
-      img: true
+      img: true,
+      loaded: false
+
     })
     const newItem = {
       name: e.target.name.value,
@@ -35,6 +49,9 @@ class App extends Component {
       reg: e.target.reg.value,
       year: e.target.se.value,
       toggle: e.target.toggle.value,
+      num: e.target.num.value,
+      comment: e.target.comment.value,
+      gender: e.target.ge.value
     }
     let messageRef = fire.database().ref('message').orderByKey().limitToLast(100);
     fire.database().ref('message').push(newItem);
@@ -47,42 +64,67 @@ class App extends Component {
 
           <div><img src={LGOG} alt="Avatar" /></div>
         </div>
-        <form className="cards top p-3" onSubmit={this.onSubmit} >
+        <form className="cards top p-3" onSubmit={this.onSubmit} id="mySubmit" >
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Name</label>
-            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Your Name" name="name" />
+            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Your Name" name="name" required="required" />
           </div>
+
+          <div>
+            <label htmlFor="exampleInputPassword1">Gender</label>
+            <select className="custom-select" name="ge" required="required">
+              <option selected>Gender</option>
+              <option value="Male">Male</option>s
+              <option value="Female">Female</option>
+            </select>
+          </div>
+          <br></br>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address" ref="email" name="email" required pattern="^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$" />
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="SRM Email Address" ref="email" name="email" required="required" pattern="(.+@srmist.edu.in|.+@srmuniv.edu.in)" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Mobile Number</label>
+            <input type="tel" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Mobile Number" name="num" required="required" pattern="(7|8|9)\d{9}" />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Registration Number</label>
-            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Registration Number" name="reg" />
+            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Registration Number" name="reg" required="required" />
           </div>
           <div>
             <label htmlFor="exampleInputPassword1">Year</label>
-            <select className="custom-select" name="se">
+            <select className="custom-select" name="se" required="required">
               <option selected>Present Year</option>
               <option value="2">Second</option>s
               <option value="3">Third</option>
             </select>
           </div>
           <br></br>
-          <div className="custom-control custom-switch">
-            <input type="checkbox" className="custom-control-input" id="customSwitch1" name="toggle" />
-            <label className="custom-control-label" htmlFor="customSwitch1">Have you ever attended Our previous Workshop</label>
+          <div className="form-group">
+            <label htmlFor="exampleFormControlTextarea1">Why do you want to attend workshop?</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" name="comment" required="required"></textarea>
+          </div>
+          <div className="sname">
+            Have You ever attended our previos Workshop?
+            <div className="checkbox">
+              <div className="custom-control custom-switch">
+                <input type="checkbox" className="custom-control-input" id="customCheck1" name="toggle" />
+                <label className="custom-control-label" htmlFor="customCheck1"></label>
+              </div>
+            </div>
           </div>
           <br></br>
           <div className="cap">
-            <Recaptcha
-              sitekey=""
-              render="explicit"
-              theme="light"
-              size="compact"
-              onloadCallback={this.Callback}
-              verifyCallback={this.verifyCallback}
-            />
+            <div className="recaptcha">
+              <Recaptcha
+                sitekey="6LcWvs4UAAAAADdEHZiMPI2P1Z4tNogaau7Fc_i0"
+                render="explicit"
+                theme="light"
+                size="normal"
+                onloadCallback={this.Callback}
+                verifyCallback={this.verifyCallback}
+              />
+            </div>
           </div>
           <br></br>
           {
--- src/fire.js
+++ src/fire.js
@@ -1,6 +1,13 @@
 import firebase from 'firebase'
 var firebaseConfig = {
-
+    apiKey: "AIzaSyBht2FXyOp1RV6u2iRNxXU0PtfRuJSGvwg",
+    authDomain: "dscdatabase-dfbc7.firebaseapp.com",
+    databaseURL: "https://dscdatabase-dfbc7.firebaseio.com",
+    projectId: "dscdatabase-dfbc7",
+    storageBucket: "dscdatabase-dfbc7.appspot.com",
+    messagingSenderId: "329097086012",
+    appId: "1:329097086012:web:b2b26750287b0741c3ef39",
+    measurementId: "G-3D3EN19Y2H"
 };
 
 var fire = firebase.initializeApp(firebaseConfig)
