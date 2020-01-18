4fbc0a56bbe65975c5e0cf36cc83baee3dca9b04
ArpitSharma2800 <28arpit2000sharma@gmail.com>
2020-01-18 23:35:54 +0530

    Phase 6

---
+++ src/Components/Success.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import './Success.css'
+import LGOO from './moulin.gif'
+import LGO from '../download.png'
+class Success extends Component {
+
+    render() {
+        const { history } = this.props;
+        return (
+            <div className="bg">
+                < img src={LGO} alt="Avatar" />
+                < img src={LGOO} alt="Avatar" />
+                <div className="text">Registration successful</div>
+            </div>
+        )
+    }
+}
+
+export default Success
+
